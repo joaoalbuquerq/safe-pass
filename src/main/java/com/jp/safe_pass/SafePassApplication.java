7a006d06ca1c25b0d2b5a69b6432829a17b54acf
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2024-08-17 19:52:54 -0300

    initial commit

---
+++ src/main/java/com/jp/safe_pass/SafePassApplication.java
@@ -0,0 +1,13 @@
+package com.jp.safe_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/safe_pass/SafePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.safe_pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
